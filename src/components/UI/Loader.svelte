1f9df68ed4c6491ed0994f38fed2a02c1e0f66ae
ilojan <1994@loj>
2023-03-23 10:20:50 +0530

    added loader

---
+++ src/components/UI/Loader.svelte
@@ -0,0 +1,11 @@
+<div>
+    <div class="w-full h-full fixed block top-0 left-0 loader-bg  z-50">
+        <span class=" top-1/2 my-0 mx-auto block relative" style="top: 50%;color: #43946A;">
+         <div class="flex items-center justify-center">
+          <div class="spinner-border animate-spin inline-block w-12 h-12 border-4 rounded-full" role="status">
+       
+          </div>
+        </div>
+        </span>
+      </div>
+</div>
---
+++ src/routes/test-store/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+
+    import { onMount } from "svelte";
+    import { env } from '$env/dynamic/public'
+    import Loader from "../../components/UI/Loader.svelte";
+    let posts =[];
+
+    async function getData() {
+        const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+        posts = await res.json();
+        console.log(posts);
+    }
+    onMount(()=>{
+         getData();
+    })
+</script>
+{env.PUBLIC_API_KEY}
+{#each posts as post,i  }
+    <h3>{post.id} {post.title}</h3>
+    <p>{post.body}</p>
+    {:else}
+    <p>
+       <Loader/>
+    </p>
+{/each}
